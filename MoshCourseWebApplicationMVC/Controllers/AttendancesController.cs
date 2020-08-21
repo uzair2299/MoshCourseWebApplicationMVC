45e2870bfeb8f910d9e848191f5f5e476f49b0ae
UzairAnwar <uzair.anwar@phenologix.com>
2020-08-21 20:05:50 +0500

    add going or not with api

---
+++ MoshCourseWebApplicationMVC/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using MoshCourseWebApplicationMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MoshCourseWebApplicationMVC.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        [Authorize]
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var UserId = User.Identity.GetUserId();
+            if( _context.Attendances.Any(u => u.AttendeeId == UserId && u.GigId == gigId)) { 
+                return BadRequest("The attendance is alreadu exist");
+            }
+
+            Attendance attendance = new Attendance
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+        };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- MoshCourseWebApplicationMVC/Models/IdentityModels.cs
+++ MoshCourseWebApplicationMVC/Models/IdentityModels.cs
@@ -17,5 +17,12 @@ namespace MoshCourseWebApplicationMVC.Models
         }
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>().HasRequired(a => a.Gig).WithMany().WillCascadeOnDelete(false); 
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
